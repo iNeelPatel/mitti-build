7f48b3ca33488e3a67a046a0c11394d3d09ce6d2
Neel Patel <neelpatel.6531@gmail.com>
2020-03-28 18:25:17 +0530

    push build

---
+++ precache-manifest.dbf67afc05f6bcaa9b27318a5870e7b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60bc4b7fa601e464519640fe8d98ac5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc3f55bfb478755bf98e",
+    "url": "/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "7cc244257fd885818cd8",
+    "url": "/static/css/main.1a2d0176.chunk.css"
+  },
+  {
+    "revision": "dc3f55bfb478755bf98e",
+    "url": "/static/js/2.8d4db755.chunk.js"
+  },
+  {
+    "revision": "7968e82b83ec1387ff9946b1a53da14b",
+    "url": "/static/js/2.8d4db755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc244257fd885818cd8",
+    "url": "/static/js/main.90d9b89d.chunk.js"
+  },
+  {
+    "revision": "dbcdec5176238bef35b0",
+    "url": "/static/js/runtime-main.3d6995ae.js"
+  },
+  {
+    "revision": "020b222dee6367fef0f6fd0542fbf64d",
+    "url": "/static/media/logo.020b222d.png"
+  }
+]);
